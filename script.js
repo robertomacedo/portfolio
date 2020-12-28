32d86132a350ed8dbc45094c1c88fe0c88f04ecc
robertomacedo <macedorss20@gmail.com>
2020-12-28 14:30:55 -0300

    :package: conhecimentos

---
+++ script.js
@@ -0,0 +1,7 @@
+/*var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("show-menu");
+});*/
+
+document.querySelector(".hamburger").addEventListener("click", ()=>
+    document.querySelector(".container").classList.toggle("show-menu"));
